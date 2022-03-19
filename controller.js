25170170dd346beac2722cd33b17622a6f8fc5d0
0612 <hello@nerdfirst.net>
2022-03-20 00:24:21 +0800

    Added boilerplate and general file structure for main program

---
+++ controller.js
@@ -0,0 +1,3 @@
+/*	CONTROLLER.js
+	Contains code to drive the GUI, primarily for responding to user interactions and calling the appropriate functions from logic.js
+*/
---
+++ logic.js
@@ -0,0 +1,4 @@
+/*	LOGIC.js
+	Contains the actual heavy lifting logic, performing all the calculations etc
+	Should only work with strings and lists and have no coupling with the UI or controller
+*/
