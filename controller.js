edf16b76d4d03e96cf773b0e38edbc461636bd0b
0612 <hello@nerdfirst.net>
2022-03-20 21:54:20 +0800

    Designed HTML structure and supporting JS functions for frontend

--- controller.js
+++ controller.js
@@ -1,3 +1,27 @@
 /*	CONTROLLER.js
 	Contains code to drive the GUI, primarily for responding to user interactions and calling the appropriate functions from logic.js
 */
+
+// Global variables representing HTML elements
+var queryDiv
+var queryControlsDiv
+var exclusionsControlsDiv
+var musthavesControlsDiv
+
+// Global variables representing controller state
+var query = ""
+
+window.onload = function() {
+	queryDiv = document.getElementById("queryDiv")
+	queryControlsDiv = document.getElementById("queryControlsDiv")
+	exclusionsControlsDiv = document.getElementById("exclusionsControlsDiv")
+	musthavesControlsDiv = document.getElementById("musthavesControlsDiv")
+	
+	buildGUI()
+}
+
+/*	buildGUI creates and places repetitive elements into the HTML
+*/
+function buildGUI() {
+	
+}
